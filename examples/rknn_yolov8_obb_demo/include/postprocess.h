bfdbf08ba529de4caf4d7ca985066aba45d8303f
张潜 <1013429609@qq.com>
2024-07-16 10:56:20 +0800

    Update postprocess.h

--- examples/rknn_yolov8_obb_demo/include/postprocess.h
+++ examples/rknn_yolov8_obb_demo/include/postprocess.h
@@ -11,6 +11,12 @@
 typedef signed char int8_t;
 typedef unsigned int uint32_t;
 
+/***
+CLASSES = ['plane', 'ship', 'storage tank', 'baseball diamond', 'tennis court', 'basketball court',
+           'ground track field', 'harbor', 'bridge', 'large vehicle', 'small vehicle', 'helicopter', 'roundabout',
+           'soccer ball field', 'swimming pool']
+***/
+
 typedef struct
 {
     int classId;
